d33c39cf3e411d26a71bab78d76544a38aa9fc69
Jean Luc Kabulu <jeanluc.kabulu@gmail.com>
2020-06-18 17:50:42 +0200

    Add Exemple for 81 & 83 in ordinal

--- example/main.dart
+++ example/main.dart
@@ -5,7 +5,8 @@ void main() {
   print(humanize.appNumber(9)); // six
   print(humanize.intComma(100));
   print(humanize.intComma(80010000));
+  print(humanize.ordinal(1)); // 2nd
   print(humanize.ordinal(2)); // 2nd
   print(humanize.ordinal(11));
-  print(humanize.ordinal(24)); // 24th
+  print(humanize.ordinal(81)); // 24th
 }
