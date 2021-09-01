d25a9109bcc375f7ecd3de20552edcd568b23d30
Joachim Seminck <joachim@seminck.be>
2021-09-01 15:11:32 +0300

    Fix ordinal calculation, add tests

--- lib/src/humanize.dart
+++ lib/src/humanize.dart
@@ -27,56 +27,56 @@ String ordinal(int value) {
     finalValue = templates[value];
   } else {
     tempValue = value % 10;
+
     templates = {
       // Ordinal format when value ends with 0, e.g. 80th
-      "$tempValue": "${value}th",
+      "0": "${value}th",
       // Ordinal format when value ends with 1, e.g. 81st, except 11.
-      "$tempValue": "${value}st",
+      "1": "${value}st",
       // Ordinal format when value ends with 2, e.g. 82nd, except 12.
-      "$tempValue": "${value}nd",
+      "2": "${value}nd",
       // Ordinal format when value ends with 3, e.g. 83rd, except 13.
-      "$tempValue": "${value}rd",
+      "3": "${value}rd",
       // Ordinal format when value ends with 4, e.g. 84th.
-      "$tempValue": "${value}th",
+      "4": "${value}th",
       // Ordinal format when value ends with 5, e.g. 85th.
-      "$tempValue": "${value}th",
+      "5": "${value}th",
       // Ordinal format when value ends with 6, e.g. 86th.
-      "$tempValue": "${value}th",
+      "6": "${value}th",
       // Ordinal format when value ends with 7, e.g. 87th.
-      "$tempValue": "${value}th",
+      "7": "${value}th",
       // Ordinal format when value ends with 8, e.g. 88th.
-      "$tempValue": "${value}th",
+      "8": "${value}th",
       // Ordinal format when value ends with 9, e.g. 89th.
-      "$tempValue": "${value}th",
+      "9": "${value}th",
     };
     finalValue = templates["$tempValue"];
   }
   return finalValue;
 }
 
-
 /// Convert an integer to a string containing commas every three digits.
 /// For example, 3000 becomes '3,000' and 45000 becomes '45,000'.
 String intComma(int value) {
-   // Convert int value to a String one
-   String valueToString = '$value';
+  // Convert int value to a String one
+  String valueToString = '$value';
 
-   // Use RegExp function witch used to match strings or parts of strings
-   RegExp reg = new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
-   Function mathFunc = (Match match) => '${match[1]},';
-   
-   // Store the result in a new String value
+  // Use RegExp function witch used to match strings or parts of strings
+  RegExp reg = new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+  Function mathFunc = (Match match) => '${match[1]},';
 
-   String newValue = valueToString.replaceAllMapped(reg, mathFunc);
+  // Store the result in a new String value
 
-   return '$newValue';
+  String newValue = valueToString.replaceAllMapped(reg, mathFunc);
+
+  return '$newValue';
 }
 
 /// Convert a large integer to a friendly text representation. Works best
 /// for numbers over 1 million. For example, 1000000 becomes '1.0 million',
 /// 1200000 becomes '1.2 million' and '1200000000' becomes '1.2 billion'.
 String intWord(int value) {
-  if (value < 1000000){
+  if (value < 1000000) {
     return "$value";
   }
   // TODO : Implement numbers Hight to 1M
@@ -85,23 +85,12 @@ String intWord(int value) {
 /// For numbers 1-9, return the number spelled out. Otherwise, return the
 /// number. This follows Associated Press style.
 String appNumber(int value) {
-  
-  List values = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine"
-  ];
+  List values = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
   if (value < 0 || value > 10) {
     return "$value";
-  }else{
-      return values[value-1];
+  } else {
+    return values[value - 1];
   }
 }
 
@@ -117,4 +106,3 @@ String naturalDay(DateTime value) {
 String naturalTime(DateTime value) {
   return "";
 }
- 
--- test/humanize_test.dart
+++ test/humanize_test.dart
@@ -4,9 +4,21 @@ import 'package:humanize/humanize.dart' as humanize;
 
 void main() {
   test('Humanize', () {
-    expect(humanize.ordinal(2), "two");
-    humanize.ordinal(1);
-    
+    expect(humanize.ordinal(1), "1st");
+    expect(humanize.ordinal(2), "2nd");
+    expect(humanize.ordinal(3), "3rd");
+    expect(humanize.ordinal(4), "4th");
+    expect(humanize.ordinal(10), "10th");
+    expect(humanize.ordinal(11), "11th");
+    expect(humanize.ordinal(12), "12th");
+    expect(humanize.ordinal(13), "13th");
+    expect(humanize.ordinal(14), "14th");
+    expect(humanize.ordinal(20), "20th");
+    expect(humanize.ordinal(21), "21st");
+    expect(humanize.ordinal(22), "22nd");
+    expect(humanize.ordinal(23), "23rd");
+    expect(humanize.ordinal(24), "24th");
+
     // final calculator = Calculator();
     // expect(calculator.addOne(2), 3);
     // expect(calculator.addOne(-7), -6);
